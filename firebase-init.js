6b4fdfb507d3694a8989d8a328c342bf6937ec4f
Mykyta <mykyta.kovalenko@proarea.com>
2024-03-18 17:14:05 -0500

    init firebase

---
+++ firebase-init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDW-UT8mAZvofocVPL5Nl7YMwY51E_KUcc',
+  authDomain: 'coffee-lovers-cfd66.firebaseapp.com',
+  projectId: 'coffee-lovers-cfd66',
+  storageBucket: 'coffee-lovers-cfd66.appspot.com',
+  messagingSenderId: '1096778012761',
+  appId: '1:1096778012761:web:ba19dc6375eea9f1fab948',
+  measurementId: 'G-4GH35H1J2R',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
